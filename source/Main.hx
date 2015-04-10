b00caf3646be97248bf5bf6e95101cd82198906a
Chris K <christopherjkim@gmail.com>
2015-04-10 18:09:27 -0400

    skip to PlayState unless (#if debug); else use MenuState

--- source/Main.hx
+++ source/Main.hx
@@ -12,7 +12,11 @@ class Main extends Sprite
 {
   var gameWidth:Int = 512; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
   var gameHeight:Int = 288; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-  var initialState:Class<FlxState> = PlayState; // The FlxState the game starts with.
+  #if debug
+    var initialState:Class<FlxState> = PlayState; // The FlxState the game starts with.
+  #else
+    var initialState:Class<FlxState> = MenuState;
+  #end
   var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
   var framerate:Int = 60; // How many frames per second the game should run at.
   var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
